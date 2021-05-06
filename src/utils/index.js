4d8b3307233d9f81c3fbd867f04117c4922e6cc0
zakkor <edward.partenie@gmail.com>
2021-05-07 00:05:23 +0300

    Ignore __dirty__ and __initial__

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,13 @@
 export const isDate = d => d instanceof Date;
 export const isEmpty = o => Object.keys(o).length === 0;
 export const isObject = o => o != null && typeof o === 'object';
-export const properObject = o => isObject(o) && !o.hasOwnProperty ? { ...o } : o;
+export const properObject = o => {
+  if (isObject(o)) {
+    delete o.__initial__;
+    delete o.__dirty__;
+    if (!o.hasOwnProperty) {
+      return { ...o };
+    }
+  }
+  return o;
+}
